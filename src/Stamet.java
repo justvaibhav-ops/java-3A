774be20645c0b9a9e5bc79a5533c15cb0f1a7f46
Vaibhav <justvaibhav72@gmail.com>
2026-09-13 19:27:06 +0530

    Just testing Static Method

---
+++ src/Stamet.java
@@ -0,0 +1,14 @@
+public class Stamet {
+    static void greet(){
+        System.out.println("Helllo Namaste");
+    }
+    void say(){
+        System.out.println("My name is Vaibhav");
+    }
+
+    static void main() {
+        greet();
+        Stamet sc=new Stamet();
+        sc.say();
+    }
+}
